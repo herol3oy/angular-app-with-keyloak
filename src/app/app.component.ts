5b898a135653d4ca74fd31a0ebbb3f56af538973
Hamed Sedighi <hamedsedighi@gmail.com>
2021-04-20 01:11:00 +0200

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app-with-keyloak';
+  title = 'angular-keycloak';
+
+  constructor(private router: Router) {
+  }
+  login() {
+    this.router.navigateByUrl('/login');
+  }
 }
